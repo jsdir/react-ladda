9f5bbbbac0f2db598b3c10709ddcfed7b0f44927
eldar <sael@ciklum.com>
2017-04-24 14:12:43 +0300

    Update component to use external prop-types instead of React

--- src/LaddaButton.jsx
+++ src/LaddaButton.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from "prop-types";
 import Ladda from 'ladda'
 
 import { SIZES, STYLES } from './constants'
