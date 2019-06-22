fbb1befe1c44f6639ca0c6753dba0c7768b56af0
hamidreza kazemi <hr.kazemi11@gmail.com>
2019-06-22 16:28:50 +0430

    Added type definitions for react-ladda

---
+++ react-ladda/react-ladda.d.ts
@@ -0,0 +1,14 @@
+// Type definitions for react-ladda v6.0.0
+// Project: https://github.com/jsdir/react-ladda
+// Definitions by: Hamidreza Kazemi <https://github.com/hrkazemi/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="../react/react.d.ts"/>
+
+declare module "react-ladda" {
+
+    import LaddaButton from './LaddaButton'
+
+    export default LaddaButton
+    export * from './constants'
+}
